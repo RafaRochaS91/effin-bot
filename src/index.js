2bfe497479cd443a5a81a6c1a5cbba2b13262afa
Leonardo Grade <gradeleonardo@gmail.com>
2021-02-17 17:59:20 +0100

    feat: initial setup for saturn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,81 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
+import dotenv from "dotenv";
 
-const website = 'https://saturn.de';
+dotenv.config();
+
+const website =
+  "https://www.saturn.de/de/product/_hama-dust-ex-55-2401071.html";
 
 (async function () {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto(website);
+  await page.goto("https://www.saturn.de/de/myaccount");
+
+  const acceptCookiesButton = await page.waitForSelector(
+    "#privacy-layer-accept-all-button",
+    { visible: true }
+  );
+  await acceptCookiesButton.click();
+
+  await page.focus("#mms-login-form__email");
+  await page.keyboard.type(process.env.SATURN_USERNAME);
+
+  await page.focus("#mms-login-form__password");
+  await page.keyboard.type(process.env.SATURN_PASSWORD);
+
+  await page.click("#mms-login-form__login-button");
+  await page.waitForResponse((response) => response.ok());
+
+  await page.goto(website, { waitUntil: "load" });
+
+  let isAvailable = false;
+  while (!isAvailable) {
+    const addToCartButton = await page.$("#pdp-add-to-cart-button");
+
+    if (addToCartButton) {
+      console.log("✅");
+
+      isAvailable = true;
+
+      await page.screenshot({ path: "available.png", fullPage: true });
+      await addToCartButton.click();
+
+      await page.goto("https://www.saturn.de/checkout/summary", {
+        waitUntil: "networkidle0",
+      });
+
+      await page.screenshot({ path: "pay.png", fullPage: true });
+
+      const [, payButton] = await page.$x(
+        "//div[@data-test]/button[contains(text(), 'Fortfahren und bezahlen')]"
+      );
+
+      await payButton.click();
+
+      await page.screenshot({ path: "pay2.png", fullPage: true });
+
+      await page.waitForSelector("#MMSKKNr");
+
+      await page.focus("#MMSKKNr");
+      await page.keyboard.type(process.env.CARD_NUMBER);
+
+      await page.focus("#MMSExpiry");
+      await page.keyboard.type(process.env.CARD_EXPIRATION);
+
+      await page.focus("#MMSCCCVC");
+      await page.keyboard.type(process.env.CARD_CVC);
+
+      await page.focus("#MMScreditCardHolder");
+      await page.keyboard.type(process.env.CARD_HOLDER);
+
+      await page.click("#submitButton");
+
+      console.log("Purchased!");
+    } else {
+      console.log("❌");
+      await page.waitForTimeout(10000);
+      await page.reload({ waitUntil: "load" });
+    }
+  }
 })();
